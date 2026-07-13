a293ab6ca1548cac35da3f2c40ee79d5427efb5b
Clint Chance <clintchance@gmail.com>
2026-07-13 12:09:36 -0500

    Revert "Make space for dPMR."
    This reverts commit 9720c7a40d376acdb43bb95488d83ea335174cbe.

--- M17CRC.h
+++ M17CRC.h
@@ -1,5 +1,5 @@
 /*
- *   Copyright (C) 2015,2016,2018,2020,2021,2023 by Jonathan Naylor G4KLX
+ *   Copyright (C) 2020,2021 by Jonathan Naylor G4KLX
  *
  *   This program is free software; you can redistribute it and/or modify
  *   it under the terms of the GNU General Public License as published by
@@ -16,40 +16,19 @@
  *   Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
  */
 
-#if !defined(SYNC_H)
-#define	SYNC_H
+#if !defined(M17CRC_H)
+#define	M17CRC_H
 
-#include "Defines.h"
+#include <cstdint>
 
-class CSync
+class CM17CRC
 {
 public:
-#if defined(USE_DSTAR)
-	static void addDStarSync(unsigned char* data);
-#endif
-
-#if defined(USE_DMR)
-	static void addDMRDataSync(unsigned char* data, bool duplex);
-	static void addDMRAudioSync(unsigned char* data, bool duplex);
-#endif
-
-#if defined(USE_YSF)
-	static void addYSFSync(unsigned char* data);
-#endif
-
-#if defined(USE_P25)
-	static void addP25Sync(unsigned char* data);
-#endif
-
-#if defined(USE_NXDN)
-	static void addNXDNSync(unsigned char* data);
-#endif
-
-	static void addM17LinkSetupSync(unsigned char* data);
-	static void addM17StreamSync(unsigned char* data);
-	static void addM17EOTSync(unsigned char* data);
+	static bool checkCRC16(const unsigned char* in, unsigned int nBytes);
+	static void encodeCRC16(unsigned char* in, unsigned int nBytes);
 
 private:
+	static uint16_t createCRC16(const unsigned char* in, unsigned int nBytes);
 };
 
 #endif
